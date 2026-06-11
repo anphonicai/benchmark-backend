05c5e9f3680b1a63076e4947e7e069cba27f26fc
Akshita <akshita.k@alchemyx.io>
2026-06-11 17:16:24 +0530

    Add Lenis smooth scrolling, synced with GSAP ScrollTrigger

--- Multi-page client data form/src/app/App.tsx
+++ Multi-page client data form/src/app/App.tsx
@@ -1,6 +1,31 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const tick = (time: number) => lenis.raf(time * 1000);
+    gsap.ticker.add(tick);
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove(tick);
+    };
+  }, []);
+
   return <RouterProvider router={router} />;
 }
